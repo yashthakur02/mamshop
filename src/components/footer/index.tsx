2180bdebf002f228f622aa9dd50f30e328b91db9
Yash Thakur <thakuryashrajsingh574@gmail.com>
2024-07-09 14:25:51 +0530

    footer button

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -9,9 +9,9 @@ const Footer = (props: Props) => {
     return (
         <footer className='bg-violet-300/20 p-5 backdrop-blur-md'>
             <div className='flex flex-col justify-center items-center space-y-3'>
-                <Button asChild variant={'destructive'} size={"icon"}>
+                <Button asChild variant={'destructive'} size={"icon"} className='p-1'>
                     <Link href={'/admin/all-games'}>
-                        <HomeIcon />
+                        <HomeIcon className='h-5 w-5' />
                     </Link>
                 </Button>
                 <p className='text-sm text-gray-600 text-center'>Copyright ©2024, Mamshop.com. ALL RIGHTS RESERVED 2024</p>
